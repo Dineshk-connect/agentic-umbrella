07783d4c7e9e715f7e43c067c8e6a50b38e31d68
Dineshk-connect <dinesh.k.connect@gmail.com>
2026-03-28 23:57:34 +0530

    fix deployment

--- server/index.js
+++ server/index.js
@@ -3,3 +3,7 @@ import app from './src/app.js'
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`))
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
