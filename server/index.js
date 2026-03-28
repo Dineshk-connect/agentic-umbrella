f0f513b271cf67f1822205591928273943dd63c2
Dineshk-connect <dinesh.k.connect@gmail.com>
2026-03-29 00:08:48 +0530

    fix host binding for railway

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ import app from './src/app.js'
 
 const PORT = process.env.PORT || 5000;
 
-console.log("PORT VALUE:", process.env.PORT);
-
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
